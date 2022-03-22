1f0905a2f9d9bc37f608037a0cd055f246cbf8fe
John. Oliveira <jcleytonuber@gmail.com>
2022-03-22 14:17:46 -0300

    Model Anuncio adicionado

---
+++ Model/Anuncio.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UmTempoEmCasaReactC.Model
+{
+    public class Anuncio
+    {
+
+        public int AnuncioID { get; set; }
+
+        
+
+
+        [Display(Name = "CEP do Imovel")]
+        public int ImovelForeignKey { get; set; }
+       
+
+        public DateTime inicio { get; set; }
+
+        [Required(ErrorMessage = "Informe o valor do Anuncio")]
+        [Display(Name = "Valor")]
+        public float valor { get; set; }
+    }
+}
